7b1fb69ad202137f1b1b645850ab2027e502bcbd
ivan malechkin <scorpunduk@yandex.ru>
2021-08-18 19:50:40 +0300

    330, 332 added

--- MainProject/15_ToolsPanelStatusBar/330_MoveTheToolBar.cs
+++ MainProject/15_ToolsPanelStatusBar/330_MoveTheToolBar.cs
@@ -4,9 +4,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
 namespace MainProject._15_ToolsPanelStatusBar
 {
-    class _330_MoveTheToolBar
+    [PetzoldExampleProject(chapterNumber: 15, page: 330)]
+    public class MoveTheToolBar : Window
     {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new MoveTheToolBar());
+        }
+
+        public MoveTheToolBar()
+        {
+            Title = "Move the Toolbar";
+
+            //Создание объекта  DockPanel как содержимого окна
+            DockPanel dock = new DockPanel();
+            Content = dock;
+
+            // Создание областей ToolBarTray у левого и верхнего края
+            ToolBarTray trayTop = new ToolBarTray();
+            dock.Children.Add(trayTop);
+            DockPanel.SetDock(trayTop, Dock.Top);
+            ToolBarTray trayLeft = new ToolBarTray();
+            trayLeft.Orientation = Orientation.Vertical;
+            dock.Children.Add(trayLeft);
+            DockPanel.SetDock(trayLeft, Dock.Left);
+
+            // Создание объекта TextBox, заполняющего остальную площадь
+            // клиентской области
+            TextBox txtBox = new TextBox();
+            dock.Children.Add(txtBox);
+
+            // Создание 6 панелей инструментов
+            for (int i = 0; i < 6; i++)
+            {
+                ToolBar toolBar = new ToolBar();
+                toolBar.Header = "Toolbar" + (i + 1);
+                if (i < 3) trayTop.ToolBars.Add(toolBar);
+                else trayLeft.ToolBars.Add(toolBar);
+
+                // ... и шести кнопок на каждой панели
+                for (int j = 0; j < 6; j++)
+                {
+                    Button btn = new Button();
+                    btn.FontSize = 16;
+                    btn.Content = (char)('A' + j);
+                    toolBar.Items.Add(btn);
+                }
+            }
+        }
     }
 }
--- MainProject/15_ToolsPanelStatusBar/332_FormatRichText.cs
+++ MainProject/15_ToolsPanelStatusBar/332_FormatRichText.cs
@@ -4,9 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
 namespace MainProject._15_ToolsPanelStatusBar
 {
-    class _332_FormatRichText
+    [PetzoldExampleProject(chapterNumber: 15, page: 332)]
+    public partial class FormatRichText : Window
     {
+        RichTextBox txtbox;
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new FormatRichText());
+        }
+
+
+        public FormatRichText()
+        {
+            Title = "Format Rich Text";
+            // Создание объекта DockPanel как содержимого окна
+            DockPanel dock = new DockPanel();
+            Content = dock;
+
+            // Создание области ToolBarTray у верхнего края окна
+            ToolBarTray tray = new ToolBarTray();
+            dock.Children.Add(tray);
+            DockPanel.SetDock(tray, Dock.Top);
+
+            // Создание объекта RichTextBox
+            txtbox = new RichTextBox();
+            txtbox.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+
+            // Вызов методов из других файлов
+            AddFileToolBar(tray, 0, 0);
+            AddEditToolBar(tray, 1, 0);
+            AddCharToolBar(tray, 2, 0);
+            AddParaToolBar(tray, 2, 1);
+            AddStatusBar(dock);
+
+            // Создание объекта RichTextBox, заполняющего 
+            // остальную площадь, и передача ему фокуса
+            dock.Children.Add(txtbox);
+            txtbox.Focus();
+        }
+
     }
 }
